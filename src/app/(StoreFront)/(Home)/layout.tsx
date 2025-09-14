77949db6ba8bf0c8e5b4bea95eba77642a11b2f1
Niaj-FreshNet <pensiveniaj@gmail.com>
2025-09-15 02:54:43 +0600

    merged

--- src/app/(StoreFront)/(Home)/layout.tsx
+++ src/app/(StoreFront)/(Home)/layout.tsx
@@ -1,5 +1,5 @@
 import AutoIDGenFooter from "@/components/layout/Footer";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/layout/Navbar";
 import { ReactNode } from "react";
 
 export default function layout({ children }: { children: ReactNode }) {

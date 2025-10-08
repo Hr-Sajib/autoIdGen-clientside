08a82bd3fe4e25c91556958f28c3807c46987f1f
web-hasib <hasibulislam9284@gmail.com>
2025-10-08 12:42:18 +0600

    hydration error fixed

--- src/app/(Dashboard)/layout.tsx
+++ src/app/(Dashboard)/layout.tsx
@@ -1,18 +1,44 @@
+// 'use client';
+
+// import Container from "@/components/layout/Dashboard/Container";
+// import PrivateRoute from "@/lib/providers/PrivateRoute";
+// import { ReactNode } from "react";
+
+// export default function Layout({ children }: { children: ReactNode }) {
+//     return (
+//         <PrivateRoute>
+
+//         <div className="min-h-screen bg-white">
+//             <Container>
+//                 {children}
+//             </Container>
+//         </div>
+//         </PrivateRoute>
+//     );
+// }
+
 'use client';
 
 import Container from "@/components/layout/Dashboard/Container";
 import PrivateRoute from "@/lib/providers/PrivateRoute";
-import { ReactNode } from "react";
+import { ReactNode, useState, useEffect } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) return null; // server-rendered HTML এ কিছু দেখাবে না
+
     return (
         <PrivateRoute>
-
-        <div className="min-h-screen bg-white">
-            <Container>
-                {children}
-            </Container>
-        </div>
+            <div className="min-h-screen bg-white">
+                <Container>
+                    {children}
+                </Container>
+            </div>
         </PrivateRoute>
     );
 }
